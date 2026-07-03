c9cea2c45f2a582055a95d899a70449d98fdbf12
Akbar_tech <akbaraminjonov2@gmail.com>
2026-07-03 17:59:56 +0500

    task02.py bajarildi

---
+++ task02.py
@@ -0,0 +1,11 @@
+
+def malumot_chiqar(**kwargs):
+    for kalit, qiymat in kwargs.items():
+        print(f"{kalit}: {qiymat}")
+        
+malumot_chiqar(
+    ism="Akbar",
+    yosh=17,
+    shahar="Samarqand",
+    kasb="Dasturchi"
+)
